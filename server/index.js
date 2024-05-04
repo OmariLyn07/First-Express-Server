f4e166ddb079984ecd3a3b6fa947ca66d51a5a8a
Omari <lynomari@gmail.com>
2024-05-03 21:18:25 -0400

    Added comments for readability

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
+//Import express functions and create a new express application
 const express = require('express')
 
 const app = express()
-
+//Port define
 const port = 3000
-
+//Static assets
 app.use(express.static('public'))
 
 const root = require('path').resolve('..\\express-react', '..\\client\\dist')
 app.use(express.static(root))
 
+//Default server route
 app.get('/', (req, res)=>{
     res.sendFile('index.html', {root})
 })
@@ -16,7 +18,7 @@ app.get('/', (req, res)=>{
 app.get('/test', (req, res)=>{
     res.send("Server is Operational")
 })
-
+//Run server on port specified
 app.listen(port, ()=>{
     console.log(`Server Running on Port ${port}`)
 })
